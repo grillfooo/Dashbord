e7b1a0c24eee53ea20ec9d04f969bfbdd878fe1a
garygaurav1313 <garygaurav1313@gmail.com>
2025-07-08 11:08:57 +0530

    Working on the middleware console removed

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,7 +8,6 @@ export function middleware(req: NextRequest) {
     url.pathname = "/login"
     return NextResponse.redirect(url)
   }
-console.log("JWT_SECRET:", process.env.JWT_SECRET) // undefined in Edge Runtime
   // Do NOT verify JWT here (Edge Runtime can't handle it securely)
   return NextResponse.next()
 }
